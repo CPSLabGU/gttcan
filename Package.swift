63e91e4cb5f61323aacd28fa9e1dda262443c4c1
Callum McColl <callum.mccoll1@gmail.com>
2023-11-28 15:41:51 +1000

    Remove docC workflow and package dependency.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
             targets: ["gttcan"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
     ],
     targets: [
         .target(
